61dec50c3a29299dc7589378bf0cfd54d58c4f1b
BanchaTNK <67319010047@tnk.ac.th>
2024-11-13 08:27:56 +0700

    t17

--- week5/ran.py
+++ week5/ran.py
@@ -1,14 +1,26 @@
 import t1
-c=0
-d=0
 while True:
     q=int(input("เลือกโปรแกรม 1 หรือ 2 :"))
     if q==1:
-        print("โปรแกรมเรียงเลข")
-        a=int(input('จาก :'))
-        b=int(input('ถึง :'))
-        print (t1.ef(a,b))
+        while True :
+            print("โปรแกรมเรียงเลข")
+            print('พิมพ์ 0 เพื่อต้องการออก')
+            a=int(input('จาก :'))
+            b=int(input('ถึง :'))
+            c =(t1.ef(a,b))
+            if b==0 :
+                break
+            print(f'{c}\n')
     elif q ==2 :
-        y = int(input("ใส่เลข :"))
-        c, d = t1.er(y, c, d)
-        print((c, d))
+        c=0
+        d=0
+        while True :
+            print("โปรแกรมรวมค่า")
+            print('พิมพ์ 0 เพื่อต้องการออก')
+            y = int(input("ใส่เลข :"))
+            if y == 0 :
+                print(f"ผลรวมของบวก  = {c}")
+                print(f'ผลรวมของบวก  = {d}')
+                break
+            c, d = t1.er(y, c, d)
+            print(c, d)
