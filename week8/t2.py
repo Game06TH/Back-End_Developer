2c8258dd79a61aa8281baa58fb157face633af11
BanchaTNK <67319010047@tnk.ac.th>
2024-12-03 11:30:08 +0700

    t19

--- week8/t2.py
+++ week8/t2.py
@@ -1,6 +1,25 @@
 import database as db
 mycursor = db.mydb.cursor()
-mycursor.execute("SELECT * FROM student")
-myresulf =mycursor.fetchall()
-for i in myresulf:
-    print(i)
+def show_table():
+    mycursor.execute("SHOW TABLES")
+    table = mycursor.fetchall()  
+    print('หัวข้อทั้งหมด')  
+    for i in table:
+        print(i)
+def select_all():
+    show_table()
+    tabie = input("ใส่หัวข้อที่ต้องการค้นหา :")
+    mycursor.execute(f"SELECT * FROM {tabie}")
+    myresulf =mycursor.fetchall()
+    for i in myresulf:
+        print(i)
+def select_student():
+    show_table()
+    tabie = input("ใส่หัวข้อที่ต้องการค้นหา :")
+    name = input('ป้อนชื่อที่ต้องการค้นหา :')
+    mycursor.execute(f"SELECT * FROM {tabie} where name like '%{name}%'")
+    myresulf =mycursor.fetchall()
+    for i in myresulf:
+        print(i)
+
+select_student()
